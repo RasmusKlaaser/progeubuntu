560cd3ca72af56e17640987c443cea3cc0de30cf
Rasmus Klaaser <rasmus.klaaser@voco.ee>
2023-09-08 11:40:43 +0300

    first variables

---
+++ variables.js
@@ -0,0 +1,28 @@
+// let and const
+/*
+let name = "Rasmus Klaaser "
+console.log(name)
+const surname = "Klaaser"
+surname = "King"
+*/
+
+/*
+// array
+const numbers = [1, 2, 3, 4, 5]
+console.log(numbers)
+numbers.push(6)
+console.log(numbers)
+*/
+
+/*
+//objects
+const person = {
+	firstname: "Rasmus", 
+	lastname: "Klaaser"
+}
+console.log(person)
+console.log(person.firstname)
+console.log(person.lastname)
+person.department = "IT Akadeemia"
+console.log(person)
+*/
